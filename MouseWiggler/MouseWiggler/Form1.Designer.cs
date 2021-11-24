c717410d8258baa533db1bad4bb4164b19af92df
Tully Work <tullyWork>
2021-11-24 12:49:25 +1030

    wiggle

---
+++ MouseWiggler/MouseWiggler/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace MouseWiggler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WiggleButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // WiggleButton
+            // 
+            this.WiggleButton.Location = new System.Drawing.Point(234, 152);
+            this.WiggleButton.Name = "WiggleButton";
+            this.WiggleButton.Size = new System.Drawing.Size(257, 102);
+            this.WiggleButton.TabIndex = 0;
+            this.WiggleButton.Text = "Wiggle";
+            this.WiggleButton.UseVisualStyleBackColor = true;
+            this.WiggleButton.Click += new System.EventHandler(this.Wiggle_ClickAsync);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.WiggleButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button WiggleButton;
+    }
+}
+
---
+++ MouseWiggler/MouseWiggler/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace MouseWiggler
+{
+    public partial class Form1 : Form
+    {
+        //This is a replacement for Cursor.Position in WinForms
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern bool SetCursorPos(int x, int y);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+        const int MYACTION_HOTKEY_ID = 1;
+
+
+        Boolean isWiggling = false;
+        Boolean tracker = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Wiggle_ClickAsync(object sender, EventArgs e)
+        {
+            if (isWiggling)
+            {
+                isWiggling = false;
+            }
+            else
+            {
+                isWiggling = true;
+                Cursor.Position = new Point(Cursor.Position.X + 50, Cursor.Position.Y + 50);
+
+            }
+
+            this.Cursor = new Cursor(Cursor.Current.Handle);
+
+            await Task.Run(() => Wiggle());
+        }
+
+        private void Wiggle()
+        {
+            while (isWiggling)
+            {
+
+                Random random = new Random();
+
+                new Point(Cursor.Position.X, Cursor.Position.Y);
+                if (tracker)
+                {
+                    Cursor.Position = new Point(Cursor.Position.X + 10, Cursor.Position.Y);
+                    tracker = false;
+                }
+                else
+                {
+                    Cursor.Position = new Point(Cursor.Position.X - 10, Cursor.Position.Y);
+                    tracker = true;
+                }
+
+                LeftMouseClick(Cursor.Position.X, Cursor.Position.Y);
+
+
+                Thread.Sleep(2000);
+            }
+
+        }
+
+
+        //This simulates a left mouse click
+        public static void LeftMouseClick(int xpos, int ypos)
+        {
+            SetCursorPos(xpos, ypos);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, xpos, ypos, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, xpos, ypos, 0, 0);
+        }
+    }
+}
